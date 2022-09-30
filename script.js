c50c8a68cb0835e5481ac516fb389a15535b6b36
“Rebecca <rebeccaschiffman@gmail.com>
2022-09-29 17:39:13 -0700

    Submit form when enter is pressed

--- script.js
+++ script.js
@@ -37,9 +37,15 @@ const handleLocationInput = function() {
   }
 }
 
-
-
 submitBtn.addEventListener('click', handleSubmitClick);
+
+locationInput.addEventListener('keypress',function(e){
+  if (e.key === 'Enter'){
+    e.preventDefault();
+    console.log('enter was pressed');
+    handleSubmitClick();
+    }
+});
 
 locationInput.addEventListener('input', handleLocationInput);
 
@@ -124,15 +130,5 @@ const showGIF = function(keyword) {
 
 
 // OpenWeatherMap API has several types of data that you can request. To get the current weather in a specific location, you can pass in the name of a city (optionally, you can also add a state code or a country code) as a URL query string parameter, like so:
-
-// api.openweathermap.org/data/2.5/weather?q=London
-
-// Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
-
-// Set up a simple form that will let users input their location and will fetch the weather info (still just console.log() it).
-
-// Display the information on your webpage!
-
-// Add any styling you like!
 
 // Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API.
